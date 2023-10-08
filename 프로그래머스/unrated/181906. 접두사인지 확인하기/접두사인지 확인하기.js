c2ddd273558efb65596edc605ad00c34e19c43de
JI SU LEE <118723315+jisulee97@users.noreply.github.com>
2023-10-08 13:11:48 +0900

    [unrated] Title: 접두사인지 확인하기, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.js
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -1,4 +1,4 @@
 function solution(my_string, is_prefix) {
-    if(my_string.startsWith(is_prefix)) return 1;
+    if(my_string.slice(0,is_prefix.length) === is_prefix) return 1;
     else return 0;
 }
